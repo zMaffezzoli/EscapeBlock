de24f622c438d968f001e39b65f31b352e831b6b
zMaffezzoli <brunomaffezzolirodrigues0@gmail.com>
2023-08-14 22:53:02 -0300

    Problema FPS resolvido

--- game/fase3.py
+++ game/fase3.py
@@ -33,13 +33,7 @@ def fase3():
     objetivo_group.draw(tela)
 
     gerar_texto("Level 3", AMARELO, 251, 30, 20)
-
     gerar_texto("Anteontem era quarta, há 17 dias era?", BRANCO, 100, 10, 12)
-    
-    gerar_texto("Domingo", BRANCO, 105, 185, 12)
-
-    gerar_texto("Quarta", BRANCO, 295, 185, 12)
-
-    gerar_texto("Terça", BRANCO, 485, 185, 12)
+    gerar_texto("Domingo         Quarta          Terça", BRANCO, 105, 185, 12)
 
     return jogador.fase_atual
